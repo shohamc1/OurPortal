60f6b22c4a4b872bf0766b35515d6525446731fc
shohamc1 <shhmchk@gmail.com>
2021-02-10 18:42:10 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "OurPortal",
+  },
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ],
+};
