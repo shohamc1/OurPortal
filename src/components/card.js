a6e01508bc7b2f8925ef441ce541c77f1dd930cb
shohamc1 <shhmchk@gmail.com>
2021-02-17 12:32:45 +0800

    better responsiveness for cards

--- src/components/card.js
+++ src/components/card.js
@@ -7,12 +7,12 @@ const Card = ({
   instructorLastName,
 }) => {
   return (
-    <div class="flex flex-col rounded bg-gray-50 min-w-80 w-auto h-auto shadow-md overflow-hidden mr-6 mb-6">
+    <div class="flex flex-col rounded bg-gray-50 w-auto h-auto shadow-md overflow-hidden mr-6 mb-6">
       <div class="text-4xl font-semibold bg-mint px-4 py-2">{courseCode}</div>
       <div class="flex flex-col px-4 py-2">
         <span class="text-lg pb-20">{courseName}</span>
         <div class="flex">
-          <div class="flex flex-col">
+          <div class="flex flex-col md:pr-24">
             <span class="text-sm font-light">{instructorLastName}</span>
             <span class="text-sm font-light">{instructorFirstName}</span>
           </div>
