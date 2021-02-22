e1fd7aafd3cea42c6735cd4440fcc7bbdc411a63
shohamc1 <shhmchk@gmail.com>
2021-02-22 09:35:01 +0800

    updated signout button

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ const Header = ({ pageName = "Test" }) => {
   return (
     <div class="flex border-b-2 w-auto h-auto items-center">
       <h1 class="font-bold text-5xl px-8 py-4 mr-auto">{pageName}</h1>
-      <button class="rounded-button mx-8 my-4 px-4 py-1 border-purple-500 border-2 hover:bg-purple-500 align-right self-center">
+      <button class="rounded-button mx-8 my-4 px-4 py-1 primary-button-ns align-right self-center text-white">
         Sign Out
       </button>
     </div>
