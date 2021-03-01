9d83848504316223a4a6af5b582ba07eaed6e3f6
shohamc1 <shhmchk@gmail.com>
2021-03-01 14:33:28 +0800

    fix gatsby build issue

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,33 @@
+const path = require("path");
+
+exports.onCreateWebpackConfig = ({ actions, stage, getConfig }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      externals: getConfig().externals.concat(function (
+        context,
+        request,
+        callback
+      ) {
+        const regex = /^@?firebase(\/(.+))?/;
+        // exclude firebase products from being bundled, so they will be loaded using require() at runtime.
+        if (regex.test(request)) {
+          return callback(null, "umd " + request);
+        }
+        callback();
+      }),
+    });
+  }
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, "src/components"),
+        pages: path.resolve(__dirname, "src/pages"),
+        assets: path.resolve(__dirname, "src/assets"),
+      },
+    },
+  });
+};
+
 exports.onCreatePage = ({ page, actions }) => {
   const { createPage } = actions;
   // Make the front page match everything client side.
